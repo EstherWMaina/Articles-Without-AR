20fa3cd92a2e36ca7c0047fb37f5a0d38b05ddb6
Esther Maina <mainaesther77@gmial.com>
2022-10-31 02:05:24 +0300

    Added code

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,25 @@
 class Author
-  attr_accessor :name
-
+  attr_reader :name
+  include MagazinesMod
+  include ArticleMod
 
   def initialize(name)
     @name = name
-  
   end
 
+  def articles
+    @@all_articles.filter{|art| art.author.name == self.name}
+  end
 
+  def magazines
+    articles.map{|mag|mag.magazine}.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+    magazines.map{|article| article.category}
+  end
 end
