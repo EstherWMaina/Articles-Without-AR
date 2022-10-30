cbebaa42cf312605b906cecc7aad0969bf3c3725
Esther Maina <mainaesther77@gmial.com>
2022-10-31 02:07:59 +0300

    Added magazine code

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,28 @@
 class Magazine
-  attr_accessor :name, :category
 
+  include MagazinesMod
 
-  def initialize(name, category)
-    @name = name
-    @category = category
-
+  def self.all
+    @@all_magazines
   end
 
+  def contributors
+    ArticleMod.all.filter{ |mag| mag.magazine.name == @name}
+  end
+  
+  def self.find_by_name(name)
+    @@all_magazines.find{|find| find.name == name}
+  end
 
+  def article_titles
+    ArticleMod.all.filter{|article| article.magazine.name == @name}
+  end
+  
+  def contributing_authors
+    author_list = []
+    appearance = article_titles.map{|tit| tit.author.name}
+    appearance.tally.each{|key, value| value > 2 && author_list << key}
+    return author_list
+  end
+  
 end
